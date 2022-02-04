2bb33cb4fc9766a9fbc2dacc1338960529a66405
chige12 <tige1215.technic@gmail.com>
2022-02-04 15:46:14 +0900

    ✨ add hai component

---
+++ frontend/src/components/Hai/Hai.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+type HaiType = 'm'|'p'|'s'|'z'
+
+type Props = {
+  type: HaiType,
+  number: number,
+}
+
+export const Hai: React.FC<Props> = ({
+  type = 'm',
+  number = 1,
+}) => {
+
+  const isValidNumberMinMax = (min: number, max: number): boolean => {
+    return (min <= number) && (number <= max)
+  }
+
+  const isValidHaiNumber = (): boolean => {
+    switch (type) {
+      case 'z':
+        return isValidNumberMinMax(1, 7);
+      case 'm':
+      case 'p':
+      case 's':
+        return isValidNumberMinMax(1, 9);
+      default:
+        return false
+    }
+  }
+
+  const defaultHaiLink = `/images/hai/m1.png`
+  const haiLink = `/images/hai/${type}${String(number)}.png`
+
+  const haiSrc = isValidHaiNumber() ? haiLink : defaultHaiLink
+
+  return (
+    <div>
+      <img src={haiSrc} alt={`Hai-${type}-${number}`}/>
+    </div>
+  )
+};
