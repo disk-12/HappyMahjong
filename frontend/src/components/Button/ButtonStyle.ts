5cc128c2f9fb61d64832e9fa7525390e67598d89
chige12 <tige1215.technic@gmail.com>
2022-02-10 21:05:47 +0900

    🎨  CSSスタイルを調整

--- frontend/src/components/Button/ButtonStyle.ts
+++ frontend/src/components/Button/ButtonStyle.ts
@@ -5,15 +5,22 @@ const wrapperStyle = css`
   display: flex;
   align-items: center;
   padding: 8px 12px;
+  color: ${color.MainGreen};
   background-color: ${color.Surface1};
+  text-decoration: none;
+  border: none;
   border-radius: 16px;
   cursor: pointer;
+
+  &:active {
+    background-color: ${color.Selected};
+  }
 `
 
 export const ButtonWrapper = styled.button`
   ${wrapperStyle}
 `
 
-export const LinkWrapper = styled.a`
+export const LinkWrapper = styled.div`
   ${wrapperStyle}
 `
