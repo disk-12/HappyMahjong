0789750c25fea049b3215956df3f15882fcc79a3
chige12 <tige1215.technic@gmail.com>
2022-02-10 18:47:46 +0900

    ✨ Buttonコンポーネントのスタイルを分けて整理

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -1,37 +1,19 @@
 import React from "react";
+import { ButtonWrapper } from './ButtonStyle'
+
 
 interface Props {
-  border: string;
-  color: string;
-  children?: React.ReactNode;
-  height: string;
   onClick: () => void;
-  radius: string;
-  width: string;
 }
 
 const Button: React.FC<Props> = ({
-  border,
-  color,
-  children,
-  height,
   onClick,
-  radius,
-  width,
 }) => {
   return (
-    <button
+    <ButtonWrapper
       onClick={onClick}
-      style={{
-        backgroundColor: color,
-        border,
-        borderRadius: radius,
-        height,
-        width,
-      }}
     >
-      {children}
-    </button>
+    </ButtonWrapper>
   );
 };
 
---
+++ frontend/src/components/Button/ButtonStyle.ts
@@ -0,0 +1,11 @@
+import { color } from "assets/color";
+import styled from "styled-components";
+
+export const ButtonWrapper = styled.button`
+  display: flex;
+  align-items: center;
+  padding: 8px 12px;
+  background-color: ${color.Surface1};
+  border-radius: 16px;
+  cursor: pointer;
+`
