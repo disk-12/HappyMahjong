02fa687e8f1d6c02773826773213f01159e606d4
chige12 <tige1215.technic@gmail.com>
2022-02-17 01:21:15 +0900

    ✨ 符、飜、点数

--- frontend/src/pages/Result/Result.tsx
+++ frontend/src/pages/Result/Result.tsx
@@ -1,8 +1,7 @@
 // libraries
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import Riichi from "riichi";
-import { MdOutlineCameraAlt } from "react-icons/md";
+import { MdArrowForward, MdCalculate, MdOutlineCameraAlt } from "react-icons/md";
 // components
 import { Button } from "components/Button";
 import { ButtonText } from "components/Button/ButtonStyle";
@@ -12,7 +11,8 @@ import { useAppSelector } from "app/store";
 import { haiListSelector, haiListState } from "app/HaiListSlice";
 import { optionSelector, OptionState } from "app/OptionSlice";
 // style
-import { HaiBox, HaiContainer, YakuContainer, YakuBox } from "./ResultStyle";
+import { HaiBox, HaiContainer, YakuContainer, YakuBox, ResultContainer, ResultBox, FuAndHan, Ten, ButtonContainer } from "./ResultStyle";
+import { color } from "assets/color";
 
 interface Props {}
 interface ResultState {
@@ -42,7 +42,7 @@ export const Result: React.FC<Props> = () => {
     setResult(riichi.calc());
   }, [haiList, option]);
 
-  const yakuList = result ? Object.entries(result.yaku).map(([name, han]) => ({name, han})) : null
+  const yakuList = result ? Object.entries(result.yaku).map(([name, han]) => ({name, han})) : []
 
   return (
     <>
@@ -54,21 +54,39 @@ export const Result: React.FC<Props> = () => {
           </HaiBox>
         ))}
       </HaiContainer>
-      { yakuList && (
-        <YakuContainer>
-          {yakuList.map((yaku, i) => (
-            <YakuBox key={i}>
-              <span>{yaku.name}</span>
-              <span>{yaku.han}</span>
-            </YakuBox>
-          ))}
-        </YakuContainer>
-      )}
-      <Link to="/"></Link>
-      <Button to="/">
-        <MdOutlineCameraAlt size={24}/>
-        <ButtonText>新しく牌を撮る</ButtonText>
-      </Button>
+      { result && (
+         <>
+            <YakuContainer>
+              {yakuList.map((yaku, i) => (
+                <YakuBox key={i}>
+                  <span>{yaku.name}</span>
+                  <span>{yaku.han}</span>
+                </YakuBox>
+              ))}
+            </YakuContainer>
+            <ResultContainer>
+              <ResultBox>
+                <MdCalculate 
+                  size={32}
+                  color={color.Black}
+                />
+                <FuAndHan>{result.fu}符{result.han}飜</FuAndHan>
+                <MdArrowForward
+                  size={32}
+                  color={color.Black}
+                />
+                <Ten>{result.ten}<span>点</span></Ten>
+              </ResultBox>
+            </ResultContainer>
+          </>
+        )
+      }
+      <ButtonContainer>
+        <Button to="/">
+          <MdOutlineCameraAlt size={24}/>
+          <ButtonText>新しく牌を撮る</ButtonText>
+        </Button>
+      </ButtonContainer>
     </>
   );
 };
