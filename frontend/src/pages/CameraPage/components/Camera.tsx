b74d46fc556ad3824fa13ee16d7195b64cb05688
chige12 <tige1215.technic@gmail.com>
2022-02-17 17:24:43 +0900

    ✨ add cropAndResizeB64 function

--- frontend/src/pages/CameraPage/components/Camera.tsx
+++ frontend/src/pages/CameraPage/components/Camera.tsx
@@ -24,16 +24,36 @@ const errorMessages = {
   canvas: "Canvasが対応していません。",
 };
 
+const CROP_WIDTH = 512
+const CROP_HEIGHT = 512
+
 const Component: ForwardRefRenderFunction<CameraHandles> = (_, ref) => {
   const cameraRef = useRef<CameraType>(null)
   const wrapperRef = useRef<HTMLDivElement | null>(null)
   const [url, setUrl] = useState<string | undefined>(undefined)
 
-  const capture = useCallback(() => {
+  const capture = () => {
     if (!cameraRef.current) return;
     const imageSrc = cameraRef.current.takePhoto();
     setUrl(imageSrc);
-  }, [cameraRef]);
+    uploadResizeImage(imageSrc);
+  };
+
+  const uploadResizeImage = (imageSrc: string) => {
+    if(!wrapperRef.current) return null;
+    const width = wrapperRef.current.clientWidth
+    const height = wrapperRef.current.clientHeight
+    const aspectRatio = height / width
+    const base64 = cropAndResizeB64(imageSrc, aspectRatio)
+    console.log(base64)
+
+    // TODO: upload data:image/jpeg;base64 file
+
+  }
+
+  const cropAndResizeB64 = (imgB64_src: string, aspectRatio: number): string => {
+    return 'data:image/jpeg;base64,xxx...'
+  }
 
   const switchCamera = useCallback(() => {
     if (!cameraRef.current) return;
